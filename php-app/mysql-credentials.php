efbcf49e59d49b8732c561c4e9316f2f73e9b26b
Christian Müller <cmueller.dev@gmail.com>
2017-06-22 03:44:28 +0200

    Revert "Merge remote-tracking branch 'origin/tosca2vsphere' into parser"
    This reverts commit c2f3c9dd026110ff07e1279b0e5cfdcf117ea9ff.

--- php-app/mysql-credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "nick";
-$password = "test";
-$database = "taskdb";
-?>
---
+++ src/main/java/de/toscana/transformator/ExampleClass.java
@@ -0,0 +1,10 @@
+package de.toscana.transformator;
+
+public class ExampleClass {
+    public boolean testClass(String string) {
+        return "Hello World!".equals(string);
+    }
+}
+
+
+
