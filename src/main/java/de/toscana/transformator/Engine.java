0f0f9d1195a5fb6f04bb2d08d1860932af5bf767
Marvin Munoz Baron <mmunozba@outlook.com>
2017-06-22 19:58:15 +0200

    created dummy class with methods for engine

---
+++ src/main/java/de/toscana/transformator/Engine.java
@@ -0,0 +1,36 @@
+package de.toscana.transformator;
+
+/**
+ *  TODO: Description of Engine class
+ * @author Marvin Munoz Baron
+ */
+public class Engine {
+    private final ApplicationState applicationState;
+    private final Topology topology;
+
+    Engine(Topology topology, ApplicationState applicationState) {
+    this.topology = topology;
+    this.applicationState = applicationState;
+    }
+
+    /**
+     * TODO: Description function of create() method
+     */
+    public void create(){
+        // TODO: Implementation of create() method
+    }
+
+    /**
+     * TODO: Description of start() method
+     */
+    public void start() {
+        // TODO: Implementation of start() method
+    }
+
+    /**
+     * TODO: Description function of stop() method
+     */
+    public void stop() {
+        // TODO: Implementation of stop() method
+    }
+}
