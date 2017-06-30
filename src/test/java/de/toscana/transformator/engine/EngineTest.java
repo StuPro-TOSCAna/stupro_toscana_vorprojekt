bccac650d1a8a73ea1e87505c26c0de24833b63b
Marvin Munoz Baron <mmunozba@outlook.com>
2017-06-30 17:16:49 +0200

    created test topology

--- src/test/java/de/toscana/transformator/engine/EngineTest.java
+++ src/test/java/de/toscana/transformator/engine/EngineTest.java
@@ -1,6 +1,7 @@
 package de.toscana.transformator.engine;
 import de.toscana.transformator.model.*;
-
+import org.junit.Test;
+import java.io.IOException;
 /**
  * This class is used to test the functionality of the Engine and Creator classes.
  *
@@ -8,7 +9,17 @@ import de.toscana.transformator.model.*;
  */
 public class EngineTest {
 
-    //create an example topology
-    TOSCAli
-
+    @Test
+    public void testEngineInit() {
+        //create an example topology
+        TOSCAliteModel topology = null;
+        try {
+            topology = new TOSCAliteModel(ModelTest.readResource("valid_model_simple_task_app.xml"));
+        } catch (ParsingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Engine engine = new Engine(topology);
+    }
 }
