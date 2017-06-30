6a144eadfe7166c9e76f7b67ca589f8e7849ee3f
Marvin Munoz Baron <mmunozba@outlook.com>
2017-06-30 16:07:55 +0200

    create a class to test the engine

---
+++ src/test/java/de/toscana/transformator/engine/EngineTest.java
@@ -0,0 +1,14 @@
+package de.toscana.transformator.engine;
+import de.toscana.transformator.model.*;
+
+/**
+ * This class is used to test the functionality of the Engine and Creator classes.
+ *
+ * @author Marvin Munoz Baron
+ */
+public class EngineTest {
+
+    //create an example topology
+    TOSCAli
+
+}
