dc1a5d58fa59037411d6e1146d5da38beef94930
Nick Fode <nickfode@posteo.de>
2017-07-03 23:33:58 +0200

    Removed some files

--- .idea/fileTemplates/internal/Class.java
+++
@@ -1,6 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-import org.slf4j.*;
-#parse("File Header.java")
-public class ${NAME} {
-    private static final Logger LOG = LoggerFactory.getLogger(${NAME}.class);
-}
