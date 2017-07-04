824748d6bc84d7a76373a94d8e59bb284db8301a
Nick Fode <nickfode@posteo.de>
2017-07-04 04:56:43 +0200

    Added tool that converts all crlf to lf

--- src/main/java/de/toscana/transformator/executor/SSHConnection.java
+++ src/main/java/de/toscana/transformator/executor/SSHConnection.java
@@ -219,6 +219,8 @@ public class SSHConnection implements Executor {
         sendCommand(getRootEscalation() + "apt-get install -y unzip");
         String zip = sendCommand("unzip -o " + zipFile.getName());
         sendCommand("chmod -R +x *");
+        sendCommand(getRootEscalation() + "apt-get install -y dos2unix");
+        sendCommand("grep -URl $'\\r' . | xargs dos2unix");
         return zip;
     }
 
